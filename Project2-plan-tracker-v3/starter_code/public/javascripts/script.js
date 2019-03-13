cb0511e6d910adcf74a6e249fcf263e3c3504bac
mrosas22 <miller.rosas22@gmail.com>
2019-03-13 12:36:52 -0400

    Modified Progress Chart

--- Project2-plan-tracker-v3/starter_code/public/javascripts/script.js
+++ Project2-plan-tracker-v3/starter_code/public/javascripts/script.js
@@ -1,44 +1,35 @@
-const stockInfo  = axios.create({
+
+const routineInfo  = axios.create({
   baseURL: 'http://localhost:3000/api/',
 }); 
  
- //Get Character Form
+ //Get Data Form API =====> http://localhost:3000/api/data
  document.getElementById("getButton").onclick = function(event){
-  stockInfo.get('data')
+  routineInfo.get('data')
     .then(response => {
-        console.log('Response from the Script is: ', response.data);
-        // const countryName = response.data[0].calories;
-        // const countryCapital = response.data[0].water;
-        // const currencyName  = response.data[0].sleep
         // The following lane hides the form to create a new character when we are updating one
-        document.getElementById("character-form").style.display = "none";
-        document.getElementById("updateForm").style.display = "block";
-        // document.getElementById("countryName").innerHTML = "Calories: " + countryName;
-        // document.getElementById("countryCapital").innerHTML = "Capital: " + countryCapital;
-        // document.getElementById("currency").innerHTML = "Sleep: " + currencyName;  
+        document.getElementById('routine-form').style.display = 'none';
+        document.getElementById('routine-chart').style.display = 'block';
         printTheChart(response.data);
     })
     .catch( error => {
       console.log(error);
     });
-    const printTheChart = (stockData => {
-      console.log('The response after printChart is: ', stockData)
+    const printTheChart = (routineData => {
       // instead in the console, show data in the browser using JS DOM manipulation:
 
-      const stockLabels = stockData.map( element => element.session);
-      const stockPrice  = stockData.map( element => element.calories);
-      console.log('The array of calories is: ', stockPrice)
-      console.log('The array of sessions is: ', stockLabels)
-      const ctx         = document.getElementById('updateForm').getContext('2d');
+      const sessions  = routineData.map( element => element.session);
+      const calories  = routineData.map( element => element.calories);
+      const ctx         = document.getElementById('routine-chart').getContext('2d');
       const chart       = new Chart(ctx, {
         type: 'bar',
         data: {
-          labels: stockLabels,
+          labels: sessions,
           datasets: [{
             label: "Progress Chart",
             backgroundColor: 'rgb(255, 99, 132)',
             borderColor: 'rgb(255, 99, 132)',
-            data: stockPrice,
+            data: calories,
           }]
         }
       });
